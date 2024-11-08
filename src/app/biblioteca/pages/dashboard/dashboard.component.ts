c6d45c9ba8a5fb49207a2876af8eeab70fc3c979
Boset <boss-x86@protonmail.com>
2024-11-08 02:25:58 -0600

    patch commit

--- src/app/biblioteca/pages/dashboard/dashboard.component.ts
+++ src/app/biblioteca/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 // Angular Material
@@ -8,7 +8,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
-
 
 const MATERIAL_MODULES = [
   MatSidenavModule,
@@ -30,19 +29,35 @@ const MATERIAL_MODULES = [
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   
+  ngOnInit(): void {
+    //this.showDashboard = true;
+  }
+
   constructor(
     private router: Router
   ) {}
 
+  // page : string = "dashboard";
+
+  showDashboard: boolean = true;
+  
+  switch(){
+    if(this.router.url == '/library'){
+      this.showDashboard = true;
+    } else {
+      this.showDashboard = !this.showDashboard;
+    }
+  }
+
   options = [
-    { name: 'Ver Libros', icon: '/client/src/assets/icons/view_books.png', link: '/library/books' },
-    { name: 'Gestión', icon: 'assets/icons/management.png', link: '/library/manage' },
-    { name: 'Retornar Libro', icon: 'assets/icons/return_book.png', link: '/library/return' },
-    { name: 'Miembros', icon: 'assets/icons/members.png', link: '/library/members' },
-    { name: 'Préstamos', icon: 'assets/icons/loans.png', link: '/library/loans' },
-    { name: 'Ajustes', icon: 'assets/icons/settings.png', link: '/library/settings' }
+    { name: 'Ver Libros', icon: 'icons/view_books.png', link: '/library/books' },
+    { name: 'Gestión de Biblioteca', icon: 'icons/manage_books.png', link: '/library/manage' },
+    { name: 'Retornar Libro', icon: 'icons/return_books.png', link: '/library/return' },
+    { name: 'Miembros', icon: 'icons/members_books.png', link: '/library/members' },
+    { name: 'Préstamos', icon: 'icons/loans_books.png', link: '/library/loans' },
+    { name: 'Ajustes', icon: 'icons/settings.png', link: '/library/settings' }
   ];
 
   navigateTo(link: string) {
