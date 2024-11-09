388af8865e3af471061d0de0cc38b4756067dca0
Guillermo1804 <memosanchez101@gmail.com>
2024-11-08 19:52:45 -0600

    pruueba1

---
+++ client/.angular/cache/18.2.10/client/vite/deps_ssr/@angular_material_form-field.js
@@ -0,0 +1,46 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  MAT_ERROR,
+  MAT_FORM_FIELD,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MAT_PREFIX,
+  MAT_SUFFIX,
+  MatError,
+  MatFormField,
+  MatFormFieldControl,
+  MatFormFieldModule,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix,
+  getMatFormFieldDuplicatedHintError,
+  getMatFormFieldMissingControlError,
+  getMatFormFieldPlaceholderConflictError,
+  matFormFieldAnimations
+} from "./chunk-XHUPJP6I.js";
+import "./chunk-HJGB2GT3.js";
+import "./chunk-N74MIFLZ.js";
+import "./chunk-DQZ2PILJ.js";
+import "./chunk-TKQWSO4T.js";
+import "./chunk-2H3L6IVL.js";
+import "./chunk-NQ4HTGF6.js";
+export {
+  MAT_ERROR,
+  MAT_FORM_FIELD,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MAT_PREFIX,
+  MAT_SUFFIX,
+  MatError,
+  MatFormField,
+  MatFormFieldControl,
+  MatFormFieldModule,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix,
+  getMatFormFieldDuplicatedHintError,
+  getMatFormFieldMissingControlError,
+  getMatFormFieldPlaceholderConflictError,
+  matFormFieldAnimations
+};
+//# sourceMappingURL=@angular_material_form-field.js.map
